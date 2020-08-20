6d35510e9db37487bf2bb31b61eb8cd6b85535fa
implatfo <lehlohonolo.developer@gmail.com>
2020-08-20 10:42:33 +0200

    LM - It modifies tests and cell class to accommodate rule 2

--- tests/tests_for_question_two.py
+++ tests/tests_for_question_two.py
@@ -6,16 +6,16 @@ from src.question_two import QuestionTwo, Cell, CellState
 class TestCellWithFewerThanTwoNeighbors(unittest.TestCase):
     """Test: 'Any live cell with fewer than two live neighbours dies, as if by underpopulation.'"""
 
-    def test_cell_dies_when_only_one_neighbor(self):
-        """Test: 'Cell with one neighbour'"""
-        cell = Cell(state=CellState.ALIVE)
-        cell.get_next_state(1)
-        self.assertEquals(cell.state, CellState.DEAD)
-
     def test_cell_with_zero_neighbours_dies(self):
         """Tests 'Cell with no neighbours'"""
         cell = Cell(state=CellState.ALIVE)
         cell.get_next_state(0)
+        self.assertEquals(cell.state, CellState.DEAD)
+
+    def test_cell_dies_when_only_one_neighbor(self):
+        """Test: 'Cell with one neighbour'"""
+        cell = Cell(state=CellState.ALIVE)
+        cell.get_next_state(1)
         self.assertEquals(cell.state, CellState.DEAD)
 
 
@@ -26,6 +26,12 @@ class TestCellWith(unittest.TestCase):
         """Test: 'Cell lives on with two neighbours.'"""
         cell = Cell(state=CellState)
         cell.get_next_state(2)
+        self.assertEquals(cell.state, CellState.ALIVE)
+
+    def test_cell_lives_on_with_three_neighbours(self):
+        """Test: 'Cell lives on with three neighbours.'"""
+        cell = Cell(state=CellState)
+        cell.get_next_state(3)
         self.assertEquals(cell.state, CellState.ALIVE)
 
 
