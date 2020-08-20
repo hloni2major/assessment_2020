c74ce2e4fb3e9fca8a33e602ce776dd4ec636b33
implatfo <lehlohonolo.developer@gmail.com>
2020-08-20 10:34:02 +0200

    LM - It adds tests for the second question.

--- tests/tests_for_question_two.py
+++ tests/tests_for_question_two.py
@@ -3,20 +3,25 @@ import unittest
 from src.question_two import QuestionTwo, Cell, CellState
 
 
-class TestQuestionTwoTests(unittest.TestCase):
-    """It tests the rules of conway's game of life."""
+class TestCellWithFewerThanTwoNeighbors(unittest.TestCase):
+    """Test: 'Any live cell with fewer than two live neighbours dies, as if by underpopulation.'"""
 
     def test_cell_dies_when_only_one_neighbor(self):
-        """Tests 'Any live cell with fewer than two live neighbours dies (one neighbour), as if by underpopulation.'"""
+        """Test: 'Cell with one neighbour'"""
         cell = Cell(state=CellState.ALIVE)
         cell.get_next_state(1)
         self.assertEquals(cell.state, CellState.DEAD)
 
     def test_cell_with_zero_neighbours_dies(self):
-        """Tests 'Any live cell with fewer than two live neighbours dies (zero neighbour), as if by underpopulation'"""
+        """Tests 'Cell with no neighbours'"""
         cell = Cell(state=CellState.ALIVE)
         cell.get_next_state(0)
         self.assertEquals(cell.state, CellState.DEAD)
+
+
+class TestCellWith(object):
+    """Test: 'Any live cell with two or three live neighbours lives on to the next generation.'"""
+    pass
 
 
 if __name__ == '__main__':
